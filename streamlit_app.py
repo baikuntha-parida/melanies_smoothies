305be07358ca436bba45b1bcbbf623074eba52f1
baikuntha-parida <baikuntha.parida2025@gmail.com>
2025-10-07 16:47:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
 # Write directly to the app
 st.title(f"🥤Customize Your Smoothie!🥤 ")
@@ -54,3 +53,5 @@ if ingredients_list:
        session.sql(my_insert_stmt).collect()
         
        st.success('Your Smoothie is ordered!', icon="✅")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())

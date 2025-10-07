c39279238f4da266d9c0ec746ee21dffea4713b9
baikuntha-parida <baikuntha.parida2025@gmail.com>
2025-10-07 17:33:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ st.dataframe(data=my_dataframe,use_container_width=True)
 st.stop()
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list=st.multiselect(
     'choose up to 5 ingredients:'
